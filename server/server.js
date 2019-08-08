48c847d687485d7f591db1451ef849682f2ddc0a
nignanthomas <nignanthomas@gmail.com>
2019-08-08 10:11:38 +0200

    chore(refactor): refactoring backend code
    * move default route in its own route file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import swaggerUi from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import apiRoutes from './v1/routes';
+import defaultRoute from './v1/routes/default';
 import swaggerDocument from './api-docs/v1/swagger.json';
 
 const app = express();
@@ -25,8 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.status(200).send({ message: "Bienvenue, this is WayFarer's!!!" }));
-
+app.use('', defaultRoute);
 app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', apiRoutes);
 app.use('*', (req, res) => res.status(404).json({ status: 404, error: 'Route does not exist! or Method Not Allowed!' }));
